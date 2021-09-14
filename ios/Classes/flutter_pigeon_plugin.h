50c8b24204891b4a4a49145a43921178c7cd4d93
yuanzhiying <873965092@qq.com>
2021-09-14 17:47:49 +0800

    first commit

---
+++ ios/Classes/flutter_pigeon_plugin.h
@@ -0,0 +1,13 @@
+//
+//  flutter_pigeon_plugin.h
+//  Pods
+//
+//  Created by yuanzhiying on 2021/9/13.
+//
+
+#ifndef flutter_pigeon_plugin_h
+#define flutter_pigeon_plugin_h
+
+#import "AllTypesPigeon.h"
+
+#endif /* flutter_pigeon_plugin_h */
